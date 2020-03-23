7a73890b09c6245827fbc6f88f6af9333e4dbce7
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-23 12:47:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,8 @@ def map_to_negativize(source)
   result = []
   source.length.times do |index|
     element = source[index]
-    
-    result << 
+    mapped = element * -1
+    result << mapped
+  end
+  result
 end
